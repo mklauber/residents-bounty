8aea2f0c418bd0d2d95e3f398db253f939782357
Matthew Lauber <matt+github@mklauber.com>
2012-02-29 23:12:10 -0500

    initial units code that has attributes and methods to decorate.

--- bounty/units/__init__.py
+++ bounty/units/__init__.py
@@ -3,22 +3,28 @@ from hexmap.Map import Map, MapUnit
 class UnitError( Exception ): pass
 class MovementError( UnitError ): pass
 
+class Attribute( property ):
+	pass
+
 class Unit( object ):
 	def __init__( self, *args, **keywords ):
 		super( Unit, self ).__init__( *args, **keywords )
 		self.max_health = 100
 		self.health = 100
-		self.max_movement = 6
-		self.movement = 6
+		self.movement = 3
 
-	def move( self, target ):
-		distance = Map.distance( self.position, target )
-		if distance > self.movement:
-			raise MovementError()
+	def reset_turn( self ):
+		pass
 
-		self.position = target
-		self.movement -= distance
-		return self.movement
+	@Attribute
+	def max_health( self ):		return self.get_max_health()
+	def get_max_health( self ):	return self.max_health
+	@Attribute
+	def health( self ): 			return self.get_health()
+	def get_health( self ):		return self.health
+	@Attribute
+	def movement( self ):			return self.get_movement()
+	def get_movement( self ):	return self.movement
 
 	def heal( self, amount ):
 		healed = max( amount, self.max_health - self.health )
